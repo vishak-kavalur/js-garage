ad2ae20e4c0ef96fa0a1d087eaffc99d53196e65
vishak-kavalur <17vishak@gmail.com>
2021-07-09 20:06:19 +0530

    String reverse

--- stringReverse.js
+++ stringReverse.js
@@ -1,1 +1,27 @@
 //With helper variable
+const reverseString = (str, length)  =>  {
+    let result = ''
+    for(let i=length-1; i>=0; i--) {
+        result += str[i];
+    }
+    return result;
+}
+
+// In place with reverse()
+const reverseStringInPlace = (str) => {
+  return str.split('').reverse().join('');
+};
+
+// In place without reverse()
+const reverseStringInPlaceCustom = (str, length) => {
+  for(let i=0; i<length;i++){
+    str.splice(i,0,str.pop());
+  }
+  return str.join('');
+};
+
+let str = "String Reverse!";
+
+console.log(reverseString(str, str.length));
+console.log(reverseStringInPlace(str));
+console.log(reverseStringInPlaceCustom(str.split(''), str.length));
